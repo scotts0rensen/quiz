c648b462ca116da5124adefd543b852e4d6359ea
Scott Sorensen <scott.sorensen@octanner.com>
2014-09-22 23:14:44 -0600

    styling changes; add next button; add right/wrong classes

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 $(document).ready( function() {
 
-  $("#start").click(function () {
+  $("#start").click( function () {
     $("#intro").fadeOut("fast", function() {
       $("#quiz").fadeIn("fast");
     });
   });
 
+  $("#photo1").click( function() {
+    $("#photo1").addClass("correct");
+    $("#photo2").addClass("wrong");
+    $(".results").fadeIn("fast");
+  });
 });
